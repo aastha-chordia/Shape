9c4ff0831870f8312bddcb8cae97236fed077998
Aastha Chordia <aasthachordia595@gmail.com>
2025-03-07 21:42:26 +0530

    create the shape

--- EquilateralPyramid.java
+++ EquilateralPyramid.java
@@ -1,3 +1,8 @@
 // Aastha Chordia
 class EquilateralPyramid extends Shape implements Volume {
     private double height;
+    
+    public EquilateralPyramid(double baseSide, double height) {
+        super(baseSide, baseSide);
+        this.height = height;
+    }
