95854a3801eb77afdcbb47bb7e388c25bcfe50b1
Aastha Chordia <aasthachordia595@gmail.com>
2025-03-07 21:35:06 +0530

    Calculate the volume of sphere

--- Sphere.java
+++ Sphere.java
@@ -12,3 +12,7 @@ class Sphere extends Shape implements Volume {
         return 0; // Spheres do not have a perimeter
     }
 
+    public double calculateVolume() {
+        return (4.0 / 3.0) * Math.PI * Math.pow(dimension1, 3);
+    }
+}
