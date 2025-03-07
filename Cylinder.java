fc616474865c770372d8fa070ddd202f98f8a261
Aastha Chordia <aasthachordia595@gmail.com>
2025-03-07 21:38:08 +0530

    create a cylinder

--- Cylinder.java
+++ Cylinder.java
@@ -3,3 +3,8 @@
 
 class Cylinder extends Shape implements Volume {
     private double height;
+
+     public Cylinder(double radius, double height) {
+        super(radius, 0);
+        this.height = height;
+    }
