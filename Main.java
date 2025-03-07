6a2c28fd467ec200d28bbeda4fb07fec64610a9c
Aastha Chordia <aasthachordia595@gmail.com>
2025-03-07 22:06:44 +0530

    options for the user (menu)

--- Main.java
+++ Main.java
@@ -8,4 +8,14 @@ public class Main {
         int choice;
         
         do {
+            System.out.println("\nShape Calculation Menu:");
+            System.out.println("1. Circle");
+            System.out.println("2. Rectangle");
+            System.out.println("3. Square");
+            System.out.println("4. Sphere");
+            System.out.println("5. Cylinder");
+            System.out.println("6. Equilateral Pyramid");
+            System.out.println("7. Exit");
+            System.out.print("Enter your choice: ");
+            
             
