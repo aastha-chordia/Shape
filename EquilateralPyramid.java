66d7a52649bac08087dd183e251f3ba4e8091716
Aastha Chordia <aasthachordia595@gmail.com>
2025-03-07 22:37:22 +0530

    Final EquilateralPyramid.java

--- EquilateralPyramid.java
+++ EquilateralPyramid.java
@@ -1,24 +1,23 @@
 // Aastha Chordia
+
 class EquilateralPyramid extends Shape implements Volume {
-    private double height;
-    
     public EquilateralPyramid(double baseSide, double height) {
-        super(baseSide, baseSide);
-        this.height = height;
+        super(baseSide, height, 0, 4);
     }
-     public double calculateArea() {
-        double baseArea = dimension1 * dimension1;
-        double slantHeight = Math.sqrt((dimension1 / 2) * (dimension1 / 2) + height * height);
-        double lateralArea = 2 * dimension1 * slantHeight;
+
+    @Override
+    public double calculateArea() {
+        double baseArea = dim_one * dim_one;
+        double slantHeight = Math.sqrt((dim_one / 2) * (dim_one / 2) + dim_two * dim_two);
+        double lateralArea = 2 * dim_one * slantHeight;
         return baseArea + lateralArea;
     }
 
     public double calculatePerimeter() {
-        return 4 * dimension1;
+        return 4 * dim_one;
     }
-    
+
     public double calculateVolume() {
-        return (1.0 / 3.0) * dimension1 * dimension1 * height;
+        return (1.0 / 3.0) * dim_one * dim_one * dim_two;
     }
 }
-
