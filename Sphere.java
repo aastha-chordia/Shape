dc51c59b5bf13e50fb3220f979d09428a148ff5a
Aastha Chordia <aasthachordia595@gmail.com>
2025-03-07 21:33:43 +0530

    Calculate the area of a sphere

--- Sphere.java
+++ Sphere.java
@@ -4,3 +4,8 @@ class Sphere extends Shape implements Volume {
     public Sphere(double radius) {
         super(radius, 0);
     }
+    public double calculateArea() {
+        return 4 * Math.PI * dimension1 * dimension1;
+    }
+
+    
